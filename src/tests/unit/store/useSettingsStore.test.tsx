7bf2ad4469bed60fd85d06b02fc13f18e7a1a561
Cameron Springer <cspringer@testhero.com>
2025-02-23 21:37:29 -0500

    Tests passing

--- src/tests/unit/store/useSettingsStore.test.tsx
+++ src/tests/unit/store/useSettingsStore.test.tsx
@@ -1,5 +1,5 @@
 import { renderHook, act } from '@testing-library/react-hooks';
-import useSettingsStore from '../../store/useSettingsStore';
+import { useSettingsStore } from '../../../store/useSettingsStore';
 
 it('should clear specific filter', () => {
   const { result } = renderHook(() => useSettingsStore());
@@ -7,5 +7,5 @@ it('should clear specific filter', () => {
     result.current.setFilter('testType', 'SAT');
     result.current.clearFilter('testType');
   });
-  expect(result.current.filters.testType).toBe(''); // or expect([]) depending on your implementation
+  expect(result.current.filters.testType).toStrictEqual([]); 
 }); 
